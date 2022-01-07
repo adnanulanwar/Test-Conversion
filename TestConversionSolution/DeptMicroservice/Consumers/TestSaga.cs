6d56f3e3f95fe4a62e9a6969dbfa700050a6a136
Adnanul Anwar <anwaradnan720@gmail.com>
2022-01-07 23:19:09 +0600

    saga

---
+++ TestConversionSolution/DeptMicroservice/Consumers/TestSaga.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit.Saga;
+using SharedModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeptMicroservice.Consumers
+{
+
+    public class TestSaga : ISaga, InitiatedBy<Teacher>, Orchestrates<Student>
+    {
+        public Guid CorrelationId { get; set; }
+        public DateTime Submitted { get; set; }
+        public DateTime Completed { get; set; }
+
+        public async Task Consume(ConsumeContext<Teacher> context)
+        {
+            Submitted = context.Message.OrderDate;
+        }
+
+        public async Task Consume(ConsumeContext<Student> context)
+        {
+            Completed = context.Message.CompletionDate;
+        }
+    }
+
+}
